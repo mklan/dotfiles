15cfc8bd3b3fdbad50b994e4a003f320484fde9d
Matthias Klan <matthias.klan@gmail.com>
2018-06-28 00:31:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,21 @@
 ln -s i3/config ~/.config/i3/config
 ln -s polybar/config ~/.config/polybar/config
 ln -s rofi/config ~/.config/rofi/config
+
+
+read -p "Do you want to install the required packages (arch)?" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   installPackages
+fi
+
+function installPackages {
+  pacman -S --no-confirm \
+  i3\
+  xfce4\
+  rofi\
+  terminator\
+  polybar\
+  # ...
+}
