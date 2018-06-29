6059dab6a38f478a313f06c589492442a2a76964
mklan <matthias.klan@gmail.com>
2018-06-30 01:00:38 +0200

    links are working

--- install.sh
+++ install.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-ln -s i3/config ~/.config/i3/config
-ln -s polybar/config ~/.config/polybar/config
-ln -s rofi/config ~/.config/rofi/config
-
-
-read -p "Do you want to install the required packages (arch)?" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-   installPackages
-fi
-
 function installPackages {
-  sudo pacman -S yaourt --no-confirm
+  sudo pacman -S yaourt --noconfirm
   yaourt -Syy \
-  i3\
-  #xfce4\
-  rofi\
-  terminator\
-  polybar\
-  # ...
-  -no-confirm
+  i3 \
+  rofi \
+  terminator \
+  polybar \
+  --noconfirm
 }
+
+function createSymlinks {
+  ln -sf $pwd/i3/* ~/.config/i3/
+  ln -sf $pwd/polybar/* ~/.config/polybar/
+  ln -sf $pwd/rofi/* ~/.config/rofi/
+}
+
+function main {
+  read -p "Do you want to install the required packages (arch only) [y/N]?" -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+	installPackages
+	createSymlinks
+  fi
+}
+
+main
