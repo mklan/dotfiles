a87075d05d4990a9da5fb74dd1e60254d375b863
Matthias Klan <matthias.klan@gmail.com>
2018-09-12 15:50:05 +0200

    use aurman instead of yaourt

--- install.sh
+++ install.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-function installPackages {
-  echo ' ' >> /etc/pacman.conf
-  echo '[archlinuxfr]' >> /etc/pacman.conf
-  echo 'SigLevel = Never' >> /etc/pacman.conf
-  echo 'Server = http://repo.archlinux.fr/$arch' >> /etc/pacman.conf
-
-  sudo pacman -Sy yaourt --noconfirm --needed
-  
-  # install basic dependencies
-  sudo pacman -S --noconfirm base-devel git wget yajl
-
-  # install additional packages
-  yaourt --noconfirm --needed - < package_list.txt 
-}
-
-function createSymlinks {
-  ln -sf $(pwd)/i3/* ~/.config/i3/
-  ln -sf $(pwd)/polybar/* ~/.config/polybar/
-  ln -sf $(pwd)/rofi/* ~/.config/rofi/
-}
-
 function main {
   read -p "Do you want to install the required packages (arch only) [y/N]?" -n 1 -r
   echo
@@ -31,6 +10,30 @@ function main {
   fi
   
   createSymlinks
+}
+
+function installPackages {
+  # install basic dependencies
+  sudo pacman -S --noconfirm --needed base-devel git wget yajl
+  
+  installAurman
+
+  # install additional packages
+  aurman -S --noconfirm --needed --skip_news $(cat ./package_list.txt)
+}
+
+function createSymlinks {
+  ln -sf $(pwd)/i3/* ~/.config/i3/
+  ln -sf $(pwd)/polybar/* ~/.config/polybar/
+  ln -sf $(pwd)/rofi/* ~/.config/rofi/
+}
+
+function installAurman {
+  git clone https://aur.archlinux.org/aurman.git
+  cd aurman
+  makepkg -si --skipinteg --noconfirm --needed
+  cd ..
+  rm -rf aurman
 }
 
 main
