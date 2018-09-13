8bfb11c64a7e6accbc057f9b18db4b6766e21321
Matthias Klan <matthias.klan@eodc.eu>
2018-09-13 17:36:29 +0200

    fallback polybar display on primary display

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# get primary monitor
+export MONITOR=$(xrandr -q | grep " connected primary" | cut -d ' ' -f1)
+
 # Launch bar
 polybar bar &
 
