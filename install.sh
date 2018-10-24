f1f22a6a5366ae4b44f460f53e0d3713973729d3
Matthias Klan <matthias.klan@eodc.eu>
2018-10-24 10:56:20 +0200

    add vim config

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ function main {
   chsh -s /usr/bin/zsh $USER
 
   # install vscode pywal theme
-  git clone https://github.com/Bluedrack28/vscode-wal.git ~/.vscode/extensions/
+  git clone https://github.com/Bluedrack28/vscode-wal.git ~/.vscode/extensions/vscode-wal
 }
 
 function installPackages {
