f0c79c1a89cc794dacaf50be56360400a4b39e92
Matthias Klan <matthias.klan@eodc.eu>
2018-10-29 15:54:32 +0100

    add gtk generation to i3 startup

--- theme/generateGTKrc.sh
+++ theme/generateGTKrc.sh
@@ -7,6 +7,6 @@ source $HOME/.cache/wal/colors.sh
 #set pywal accentColor here [0-15]
 accentColor=$color1
 
-sed -i "/gtk-color-scheme = \"selected_bg_color:*/c\gtk-color-scheme = \"selected_bg_color: ${accentColor}\"" wal-arc-dark/gtk-2.0/gtkrc
+sed -i "/gtk-color-scheme = \"selected_bg_color:*/c\gtk-color-scheme = \"selected_bg_color: ${accentColor}\"" $HOME/.themes/wal-arc-dark/gtk-2.0/gtkrc
 
-sed -i "/gtk-color-scheme = \"link_color:*/c\gtk-color-scheme = \"link_color: ${accentColor}\"" wal-arc-dark/gtk-2.0/gtkrc
+sed -i "/gtk-color-scheme = \"link_color:*/c\gtk-color-scheme = \"link_color: ${accentColor}\"" $HOME/.themes/wal-arc-dark/gtk-2.0/gtkrc
