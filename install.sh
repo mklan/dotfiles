6b222a094a625b513eee4530f46039e86b64cd09
Matthias Klan <matthias.klan@gmail.com>
2018-11-17 23:30:05 +0100

    add nvm

--- install.sh
+++ install.sh
@@ -23,6 +23,8 @@ function main {
   # get vscode pywal theme
   git clone https://github.com/Bluedrack28/vscode-wal.git ~/.vscode-oss/extensions/vscode-wal
 
+  # install node version manager
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
   # install additionall apps
 
