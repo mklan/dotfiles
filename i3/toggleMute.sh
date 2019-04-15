2387721a151c2cf9bfdbb840c3f806727629ce00
Matthias Klan <matthias.klan@gmail.com>
2019-04-15 17:39:42 +0200

    random tweaks

--- i3/toggleMute.sh
+++ i3/toggleMute.sh
@@ -2,5 +2,5 @@
 
 #get active sink
 #credits: http://customlinux.blogspot.com/2013/02/pavolumesh-control-active-sink-volume.html
-activeSink=$(pactl list short sinks | grep RUNNING | cut -f1)
+activeSink=$(pactl list short sinks | grep 'RUNNING\|IDLE' | cut -f1)
 pactl set-sink-mute $activeSink toggle
