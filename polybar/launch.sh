546c1fe37c84272b24bda76def2fdf39ebd6850d
Matthias Klan <matthias.klan@gmail.com>
2019-06-30 23:44:48 +0200

    Launches polybar on all screen

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,10 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# get primary monitor
-export MONITOR=$(xrandr -q | grep " connected" | cut -d ' ' -f1)
-
-# Launch bar
-polybar bar &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar &
+  done
+else
+  polybar --reload bar &
+fi
 
 echo "Polybars launched..."
