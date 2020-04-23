56d2c7c9003d87d4e37c6a4d30e3a4a62fdc1ad3
Matthias Klan <matthias.klan@gmail.com>
2020-04-23 08:27:29 +0200

    simplify polybar lunch script

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,12 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload bar &
-  done
-else
-  polybar --reload bar &
-fi
+done
 
 echo "Polybars launched..."
