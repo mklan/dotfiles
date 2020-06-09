8bb78417cf9efff51a4f11107867d78b4c96140b
Matthias Klan <matthias.klan@gmail.com>
2020-06-09 19:04:38 +0200

    lighten docking

--- bspwm/.config/bspwm/summonworkspace.sh
+++ bspwm/.config/bspwm/summonworkspace.sh
@@ -3,12 +3,13 @@
 #alt + {1-9,0}
 #     summonworkspace.sh {1-9,10}
   D=$(bspc query -D -m | sed -n "$@ p"); \
-  M=$(bspc query --monitors --desktop $D); \
+  M=$(bspc query --monitors --desktop $@); \
   if [ $(bspc query --desktops --monitor $M | wc -l) -gt 1 ]; then \
     if [ $(bspc query --desktops --desktop focused) != $D ]; then \
-      bspc desktop $D --to-monitor focused; \
-      bspc desktop $D --focus; \
+      bspc desktop $@ --to-monitor focused; \
+      bspc desktop $@ --focus; \
     fi; \
   elif [ $(bspc query --monitors --monitor focused) != $M ]; then \
-    bspc desktop $(bspc query --monitors --desktop $D):focused --swap $(bspc query -M -m focused):focused; \
+    bspc desktop $(bspc query --monitors --desktop $@):focused --swap $(bspc query -M -m focused):focused; \
+    #bspc desktop DVI-0:focused –swap DVI-1:focused; 
   fi
