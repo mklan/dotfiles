17c065a2cba3643400e606e8be71dd58a167074e
Matthias Klan <matthias.klan@gmail.com>
2020-12-22 00:57:11 +0100

    refactoring

--- bspwm/.config/bspwm/bspwm_colors.sh
+++ bspwm/.config/bspwm/bspwm_colors.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#874f73"
-bspc config normal_border_color  "#5d364f"
+bspc config focused_border_color "#616956"
+bspc config normal_border_color  "#43483b"
