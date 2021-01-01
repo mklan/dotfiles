cd268676439b9d0dff2a41085a0e80a4773c3204
Matthias Klan <matthias.klan@gmail.com>
2021-01-01 14:43:21 +0100

    heavy refactoring

--- install.sh
+++ install.sh
@@ -6,13 +6,19 @@ function main {
   
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
-	  installList './dependencies.txt'
+	  installList './arch-pkglist'
     # install theme
     wpg-install.sh -g -b -d -i
     wpg -s ${1}
   fi
 
+ 
+  cp scripts/* /usr/bin
+
   createSymlinks
+  installZsh
+
+  ./systemd/setup.sh
   
   # disable lightdm to use startx at startup
   systemctl disable lightdm.service
@@ -22,21 +28,12 @@ function main {
   sudo systemctl enable --now lenovo_fix.service
 
 
-  # install oh my zsh
-  wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-
-  # switch to zsh
-  chsh -s /usr/bin/zsh $USER
-  # install zsh plugins
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+  # add user to video group (for light control)
+  sudo usermod -aG video $USER
+  sudo usermod -aG wheel $USER
 
   # get vscode pywal theme
-  
   # git clone https://github.com/Bluedrack28/vscode-wal.git ~/.vscode-oss/extensions/vscode-wal
-
-  # install node version manager
-  # curl -L https://git.io/n-install | bash
 
   # install additionall apps
 
@@ -45,13 +42,8 @@ function main {
 
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
-        installList './apps_list.txt'
+        installList './apps_pkglist'
   fi
-
-
-  # add user to video group (for light control)
-  sudo usermod -aG video $USER
-  sudo usermod -aG wheel $USER
 
 }
 
@@ -67,41 +59,37 @@ function installList {
   aurman -S --noedit --noconfirm --needed --skip_news $selection
 }
 
+function installZsh {
+  # install oh my zsh
+  wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+  # switch to zsh
+  chsh -s /usr/bin/zsh $USER
+  # install zsh plugins
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+}
+
 function createSymlinks {
 
+  # just in case
   mv ~/.config/neofetch/config.conf ~/.config/neofetch/config.conf.bak
-  stow neofetch
 
-  stow bspwm sxhkd polybar autorandr i3 vim rofi picom ranger wpg
+  stow neofetch bspwm sxhkd polybar autorandr i3 vim rofi picom ranger wpg
   
   ln -sf $(pwd)/_patches/mic_mute_external/lenovo-mutemic /etc/acpi/events/lenovo-mutemic
 
+ # todo: better wallpaper management
   mkdir -p ~/wallpaper
   ln -sf $(pwd)/wallpaper/* ~/wallpaper/
 
-  ln -sf $(pwd)/common/.* ~/
+  ln -sf $(pwd)/_home/.* ~/
 
   sudo mkdir -p /etc/X11/xorg.conf.d
   sudo ln -sf $(pwd)/X11/xorg.conf.d/* /etc/X11/xorg.conf.d/
 
 #  profileFolder=$(ls $HOME/.mozilla/firefox/ | grep .default)
 #  ln -sf $(pwd)/firefox/* ~/.mozilla/firefox/$profileFolder/
-
-  mkdir -p ~/.themes
-  ln -sf $(pwd)/theme/* ~/.themes/
-
-  mkdir -p $HOME/.config/wal/templates
-  ln -sf $(pwd)/wal/templates/* $HOME/.config/wal/templates/
-  
-  ln -sf $(pwd)/wal/templates/.Xdefaults $HOME/.config/wal/templates/.Xdefaults
-  ln -sf $HOME/.cache/wal/.Xdefaults ~/
-
- # ln -sf $(pwd)/wal/templates/.Xdefaults $HOME/.config/wal/templates/.Xresources
- # ln -sf $HOME/.cache/wal/.Xresources ~/
-
-  # touch $HOME/.cache/wal/dunstrc
-  # mkdir -p $HOME/.config/dunst/
-  # ln -sf $HOME/.cache/wal/dunstrc $HOME/.config/dunst/dunstrc
 
   mkdir -p ~/.config/fusuma/
   ln -sf $(pwd)/fusuma/* $HOME/.config/fusuma/
@@ -110,8 +98,7 @@ function createSymlinks {
   mkdir -p $HOME/.todo
   ln -sf $(pwd)/todo/* $HOME/.todo/
 
-  sudo mkdir -p /etc/systemd/system
-  sudo ln -sf $(pwd)/i3/i3lock.service /etc/systemd/system/i3lock.service
+
 
   sudo ln -sf $(pwd)/bluetooth/51-blueman.rules  /usr/share/polkit-1/rules.d/51-blueman.rules
 
---
+++ systemd/setup.sh
@@ -0,0 +1,2 @@
+sudo mkdir -p /etc/systemd/system
+sudo ln -sf $(pwd)/systemd/i3lock.service /etc/systemd/system/i3lock.service
