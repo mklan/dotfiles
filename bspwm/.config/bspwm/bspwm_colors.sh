16090c81ae9a6307a2cccdcbc404cc7a3907e797
Matthias Klan <matthias.klan@gmail.com>
2021-10-05 14:30:10 +0200

    reorganize config folder

--- bspwm/.config/bspwm/bspwm_colors.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-bspc config focused_border_color "#809563"
-bspc config normal_border_color  "#586644"
---
+++ config/.config/bspwm/bspwm_colors.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+bspc config focused_border_color "#807d5e"
+bspc config normal_border_color  "#585640"
