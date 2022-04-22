ca2af942db862b3f3c3769f95cdbab7de94822cf
Matthias Klan <matthias.klan@gmail.com>
2022-04-22 16:00:20 +0200

    Merge branch 'master' of github.com:mklan/dotfiles

--- config/.config/bspwm/bspwm_colors.sh
+++ config/.config/bspwm/bspwm_colors.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#807d5e"
-bspc config normal_border_color  "#585640"
+bspc config focused_border_color "#cc241d"
+bspc config normal_border_color  "#458588"
---
+++ demo.sh
@@ -0,0 +1,2 @@
+./config/.config/bspwm/spawn_float_terminal.sh gtop 1000 1200 -100 50 &
+./config/.config/bspwm/spawn_float_terminal.sh cmatrix 1000 1200 100 50 &
