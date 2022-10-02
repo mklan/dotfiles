e86d6653534ac19331ce5f14e53f121d3541e34b
Matthias Klan <matthias.klan@gmail.com>
2022-10-02 22:53:38 +0200

    faster float terminal spawn

--- config/.config/bspwm/spawn_float_terminal.sh
+++ config/.config/bspwm/spawn_float_terminal.sh
@@ -1,1 +1,2 @@
-./config/.config/bspwm/spawn_float.sh "urxvt -e zsh -ic \"$1\"" $2 $3 $4 $5
+#./config/.config/bspwm/spawn_float.sh "urxvt -e zsh -ic \"$1\"" $2 $3 $4 $5
+./config/.config/bspwm/spawn_float.sh "urxvt -e $1" $2 $3 $4 $5
