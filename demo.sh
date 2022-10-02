a719f2ac346ac57a328174e643e755f48f96be0a
Matthias Klan <matthias.klan@gmail.com>
2022-10-02 22:51:59 +0200

    [feat] open demo in desktop

--- demo.sh
+++ demo.sh
@@ -1,3 +1,4 @@
+[ ! -z "$1" ] && bspc desktop -f $1
 ./config/.config/bspwm/spawn_float_terminal.sh gtop 1000 1200 -100 50 &
 ./config/.config/bspwm/spawn_float_terminal.sh cmatrix 1000 1200 100 50 &
-# ./config/.config/bspwm/spawn_float_terminal.sh tty-clock 200 400 100 50 &
+./config/.config/bspwm/spawn_float_terminal.sh tty-clock 200 400 100 50 &
