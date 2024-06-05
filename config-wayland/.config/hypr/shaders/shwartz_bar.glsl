ce16babba0f96cf995c7de37833e52a6fee8d353
mklan <mklan@pm.me>
2024-06-05 22:18:36 +0200

    oled optimizations [WIP]

---
+++ config-wayland/.config/hypr/shaders/shwartz_bar.glsl
@@ -0,0 +1,20 @@
+precision highp float;
+varying vec2 v_texcoord;
+uniform sampler2D tex;
+
+void main() {
+    // Get the screen coordinates
+    vec2 screenCoords = gl_FragCoord.xy;
+
+    // Check if the sum of x and y coordinates is even
+    bool isEvenPixel = mod(screenCoords.x + screenCoords.y, 2.0) == 0.0;
+
+    // If it's an even pixel, keep the original color; otherwise, set it to black
+    vec4 originalColor = texture2D(tex, v_texcoord);
+    vec4 color = isEvenPixel ? originalColor : vec4(0.0, 0.0, 0.0, 1.0);
+
+    bool limit = screenCoords.y < 60.0;
+
+    // Output the final color
+    gl_FragColor = limit ? color : originalColor;
+}
---
+++ config/.cron/every60min.sh
@@ -0,0 +1,4 @@
+export DISPLAY=:0
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+
+~/.config/hypr/scripts/prevent-oled-burnin.sh
